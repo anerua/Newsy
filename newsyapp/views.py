a83d914771b89da93ad359cad5bd72132dde1cce
Martins Anerua <aneruamartins@gmail.com>
2022-01-27 16:31:43 +0100

    display received top 500 items from HN (20 only)

--- newsyapp/views.py
+++ newsyapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import http.client
+import json, time
 
 # Create your views here.
 
@@ -20,8 +21,49 @@ def get_topstories(request):
     items = items.replace("[", "")
     items = items.replace("]", "")
     
-    news_list = items.split(",")
+    items_list = items.split(",")
+
+    news_list = []
+    for item_id in items_list[0:20]:
+        news_list.append((get_item(item_id)))
 
     return render(request, "newsyapp/index.html", {"news_list": news_list})
 
 
+def get_item(item_id):
+
+    conn = http.client.HTTPSConnection("hacker-news.firebaseio.com")
+
+    payload = "{}"
+
+    conn.request("GET", f"/v0/item/{item_id.strip()}.json?print=pretty", payload)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    item = json.loads(data.decode("utf-8"))
+
+    item_time = int(item["time"])
+    elapsed_time = get_elapsed_time(item_time)
+
+    details = {"id": item["id"], "title": item["title"], "time": elapsed_time}
+    if "url" in item:
+        details["url"] = item["url"]
+
+    return details
+
+
+def get_elapsed_time(item_time):
+
+    diff_time = int(time.time()) - item_time
+
+    if diff_time / 86400 < 1:
+        if int(diff_time/3600) < 1:
+            if int(diff_time/60) < 1:
+                return f"{int(diff_time)} seconds ago"
+            else:
+                return f"{int(diff_time/60)} minutes ago"
+        else:
+            return f"{int(diff_time/3600)} hours ago"
+    else:
+        return f"{int(diff_time/86400)} days ago"
