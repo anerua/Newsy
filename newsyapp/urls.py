ead58bebd4504656fdcb3e51c56fad644cba3aae
Martins Anerua <aneruamartins@gmail.com>
2022-01-29 20:09:37 +0100

    store retrieved item in DB and display in HTML

--- newsyapp/urls.py
+++ newsyapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from newsyapp import views
 
 urlpatterns = [
-    path("", views.get_topstories, name="get_topstories"),
+    path("", views.get_stories, name="get_stories"),
+    path("sync/", views.sync_db, name="sync_db"),
 ]
--- newsyapp/views.py
+++ newsyapp/views.py
@@ -1,10 +1,99 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 import http.client
 import json, time
 
+from .models import Story, Job
+
 # Create your views here.
 
-def get_topstories(request):
+def get_stories(request):
+
+    stories = Story.objects.all()
+
+    return render(request, "newsyapp/index.html", {"stories": stories})
+
+
+def get_item(item_id):
+
+    conn = http.client.HTTPSConnection("hacker-news.firebaseio.com")
+
+    payload = "{}"
+
+    conn.request("GET", f"/v0/item/{item_id.strip()}.json?print=pretty", payload)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    item = json.loads(data.decode("utf-8"))
+
+    if item["type"] == "story":
+        # item_time = int(item["time"])
+        # elapsed_time = get_elapsed_time(item_time)
+
+        descendants = None
+        score = None
+        url = None
+        if "descendants" in item:
+            descendants = item["descendants"]
+        if "score" in item:
+            score = item["score"]
+        if "url" in item:
+            url = item["url"]
+
+        details = {"id": item["id"],
+                    "type": item["type"],
+                    "by": item["by"],
+                    "time": item["time"],
+                    # "kids": item["kids"], Use when Comments model is created
+                    "descendants": descendants,
+                    "score": score,
+                    "title": item["title"],
+                    "url": url}
+        return details
+
+    elif item["type"] == "job":
+        # item_time = int(item["time"])
+        # elapsed_time = get_elapsed_time(item_time)
+
+        text = ""
+        url = None
+        if "text" in item:
+            text = item["text"]
+        if "url" in item:
+            url = item["url"]
+
+
+        details = {"id": item["id"],
+                    "type": item["type"],
+                    "by": item["by"],
+                    "time": item["time"],
+                    # "kids": item["kids"], Use when Comments model is created
+                    "text": text,
+                    "title": item["title"],
+                    "url": url}
+        return details
+    
+    else:
+        return False
+
+def get_elapsed_time(item_time):
+
+    diff_time = int(time.time()) - item_time
+
+    if diff_time / 86400 < 1:
+        if int(diff_time/3600) < 1:
+            if int(diff_time/60) < 1:
+                return f"{int(diff_time)} seconds ago"
+            else:
+                return f"{int(diff_time/60)} minutes ago"
+        else:
+            return f"{int(diff_time/3600)} hours ago"
+    else:
+        return f"{int(diff_time/86400)} days ago"
+
+
+def sync_db(request):
 
     conn = http.client.HTTPSConnection("hacker-news.firebaseio.com")
 
@@ -22,52 +111,43 @@ def get_topstories(request):
     
     items_list = items.split(",")
 
-    news_list = []
-    for item_id in items_list[0:30]:
-        item = get_item(item_id)
-        if item:
-            news_list.append((get_item(item_id)))
+    added_no = 0
+    for item_id in items_list[:50]:
+        if item_exists(item_id, Story):
+            continue
+        if item_exists(item_id, Job):
+            continue
 
-    return render(request, "newsyapp/index.html", {"news_list": news_list})
+        response = get_item(item_id)
 
-
-def get_item(item_id):
-
-    conn = http.client.HTTPSConnection("hacker-news.firebaseio.com")
-
-    payload = "{}"
-
-    conn.request("GET", f"/v0/item/{item_id.strip()}.json?print=pretty", payload)
-
-    res = conn.getresponse()
-    data = res.read()
-
-    item = json.loads(data.decode("utf-8"))
-
-    if item["type"] not in ["job", "story"]:
-        return False
-
-    item_time = int(item["time"])
-    elapsed_time = get_elapsed_time(item_time)
-
-    details = {"id": item["id"], "title": item["title"], "time": elapsed_time}
-    if "url" in item:
-        details["url"] = item["url"]
-
-    return details
-
-
-def get_elapsed_time(item_time):
-
-    diff_time = int(time.time()) - item_time
-
-    if diff_time / 86400 < 1:
-        if int(diff_time/3600) < 1:
-            if int(diff_time/60) < 1:
-                return f"{int(diff_time)} seconds ago"
+        if response:
+            if response["type"] == "story":
+                new_story = Story(id=response["id"],
+                                    by=response["by"],
+                                    time=response["time"],
+                                    descendants=response["descendants"],
+                                    score=response["score"],
+                                    title=response["title"],
+                                    url=response["url"])
+                new_story.save()
+                added_no += 1
+            elif response["type"] == "job":
+                new_story = Job(id=response["id"],
+                                    by=response["by"],
+                                    time=response["time"],
+                                    text=response["text"],
+                                    title=response["title"],
+                                    url=response["url"])
+                new_story.save()
+                added_no += 1
             else:
-                return f"{int(diff_time/60)} minutes ago"
-        else:
-            return f"{int(diff_time/3600)} hours ago"
-    else:
-        return f"{int(diff_time/86400)} days ago"
+                print("How on Earth did you get here!!!")
+    return HttpResponse(f"Successfully added {added_no} new items to the database!")
+
+
+def item_exists(item_id, my_model):
+    try:
+        _ = my_model.objects.get(id=item_id)
+        return True
+    except my_model.DoesNotExist:
+        return False
