9937e239cbc62557b643127aa41d59625da0cc28
Martins Anerua <aneruamartins@gmail.com>
2022-01-29 16:07:37 +0100

    adds Story and Job models

--- newsyapp/models.py
+++ newsyapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Story(models.Model):
+    id = models.IntegerField(primary_key=True)
+    by = models.CharField(max_length=64, blank=True)
+    time = models.IntegerField(blank=True, null=True)
+    # kids = models.ManyToManyField("Comment", blank=True, related_name="stories") # use when Comment model is created
+    descendants = models.IntegerField(blank=True, null=True)
+    score = models.IntegerField(blank=True, null=True)
+    title = models.TextField(blank=True)
+    url = models.URLField(blank=True, null=True)
+
+
+class Job(models.Model):
+    id = models.IntegerField(primary_key=True)
+    by = models.CharField(max_length=64, blank=True)
+    time = models.IntegerField(blank=True, null=True)
+    # kids = models.ManyToManyField("Comment", blank=True, related_name="jobs") # use when Comment model is created
+    text = models.TextField(blank=True)
+    title = models.TextField(blank=True)
+    url = models.URLField(blank=True, null=True)
