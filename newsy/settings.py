520329586775ae2e7786b38ae173e034524f85ad
Martins Anerua <aneruamartins@gmail.com>
2022-02-25 17:04:18 +0100

    use django staticfilesstorage against whitenoise'

--- newsy/settings.py
+++ newsy/settings.py
@@ -135,7 +135,8 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
