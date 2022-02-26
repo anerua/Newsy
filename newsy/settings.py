e7decf88b9380d24136ecaa0f4db16508b91438f
Martins Anerua <aneruamartins@gmail.com>
2022-02-26 07:58:28 +0100

    static_root

--- newsy/settings.py
+++ newsy/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
