ec24536cd0e1df0ea1e2648a7c4eb5271e1c0332
Martins Anerua <aneruamartins@gmail.com>
2022-02-26 09:42:38 +0100

    debugging

--- newsy/settings.py
+++ newsy/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/app/static/'
+STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
