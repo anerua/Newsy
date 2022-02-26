c011fedf2121e54836dfb90688f9236c113be869
Martins Anerua <aneruamartins@gmail.com>
2022-02-26 10:38:55 +0100

    use django.contrib

--- newsy/settings.py
+++ newsy/settings.py
@@ -134,8 +134,8 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 # Default primary key field type
