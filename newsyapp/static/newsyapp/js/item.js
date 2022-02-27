6594656618e2c7167b5793206a7a168ea38edca5
Martins Anerua <aneruamartins@gmail.com>
2022-02-27 17:58:32 +0100

    format comment time to readable form

--- newsyapp/static/newsyapp/js/item.js
+++ newsyapp/static/newsyapp/js/item.js
@@ -34,7 +34,7 @@ function create_conversation(kid, level) {
 
         div.style.marginLeft = `${level * 2}%`;
         if (comment.text) {
-            div.append(`${comment.by} | ${comment.time}`);
+            div.append(`${comment.by} | ${getElapsedTime(comment.time)}`);
 
             const p1 = document.createElement('p');
             p1.innerHTML = comment.text;
@@ -51,4 +51,29 @@ function create_conversation(kid, level) {
         }
     });
     return div;
+}
+
+
+function getElapsedTime(created_time) {
+    let elapsed_seconds = Math.floor(Date.now() / 1000) - created_time;
+    
+    if (elapsed_seconds < 60) {
+        return `${elapsed_seconds} s`;
+    } else if (elapsed_seconds < 3600) {
+        return `${Math.floor(elapsed_seconds / 60)} min`;
+    } else if (elapsed_seconds < 86400) {
+        return `${Math.floor(elapsed_seconds / 3600)} h`;
+    } else if (elapsed_seconds < 2592000) {
+        let elapsed_time = Math.floor(elapsed_seconds / 86400);
+        let adj = (elapsed_time > 1) ? " days" : " day";
+        return elapsed_time + adj;
+    } else if (elapsed_seconds < 31104000) {
+        let elapsed_time = Math.floor(elapsed_seconds / 2592000);
+        let adj = (elapsed_time > 1) ? " months" : " month";
+        return elapsed_time + adj;
+    } else {
+        let elapsed_time = Math.floor(elapsed_seconds / 31104000);
+        let adj = (elapsed_time > 1) ? " years" : " year";
+        return elapsed_time + adj;
+    }
 }
